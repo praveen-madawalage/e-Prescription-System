827968f3d08dd7a7ad640c78d61418a6807105c5
Praveen Madawalage <praveenrmadawalage@gmail.com>
2025-12-17 09:46:26 +0530

    Nothing happened here.

--- src/system/models/Doctor.java
+++ src/system/models/Doctor.java
@@ -1,6 +1,6 @@
 package system.models;
 
-public class Doctor extends User{
+public class Doctor extends User {
     public Doctor (int id, String fullname, String username) {
         super(id, fullname, username);
     }
